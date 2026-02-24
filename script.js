f1b7cc144f7d361a2720755aafd46cc929f55f14
Shameen Bappy <shameenbappy3@gmail.com>
2026-02-24 20:28:48 +0600

    Create HTML calculator with display

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 //Math Operatiors Function
 
-function add(){
+function add(num1,num2){
     num1 + num2
 }
 
-function substract(){
+function substract(num1,num2){
     num1 - num2
 }
 
-function multiply(operator){
+function multiply(num1,num2){
     num1 * num2 
 }
 
-function divide(operator){
+function divide(num1,num2){
     num1 / num2
 }
 
 
+let num1;
+let num2;
+let operator;
+
+function operate(num1,num2,operator){
+    if(operator == "+" || operator == "=") add(num1,num2);
+    if(operator == "-" || operator == "=") substract(num1,num2);
+    if(operator == "*" || operator == "=") multiply(num1,num2);
+    if(operator == "/" || operator == "=") divide(num1,num2);
+}
 
