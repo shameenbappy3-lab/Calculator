aeedc03092f6520f641c4d99e622aacd4fd4b9fe
Shameen Bappy <shameenbappy3@gmail.com>
2026-02-24 19:41:02 +0600

    Create add,subtract, multiply, divide

---
+++ script.js
@@ -0,0 +1,20 @@
+//Math Operatiors Function
+
+function add(){
+    num1 + num2
+}
+
+function substract(){
+    num1 - num2
+}
+
+function multiply(operator){
+    num1 * num2 
+}
+
+function divide(operator){
+    num1 / num2
+}
+
+
+
