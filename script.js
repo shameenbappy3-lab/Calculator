b2c52001939cf91610262cc69ef9875b135d2f11
Shameen Bappy <shameenbappy3@gmail.com>
2026-02-24 21:35:54 +0600

    Configuring the Display to show user input

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 //Math Operatiors Function
 
-function add(num1,num2){
-    num1 + num2
+function add(num,num){
+    num + num
 }
 
-function substract(num1,num2){
-    num1 - num2
+function substract(num,num){
+    num - num
 }
 
-function multiply(num1,num2){
-    num1 * num2 
+function multiply(num,num){
+    num * num 
 }
 
-function divide(num1,num2){
-    num1 / num2
+function divide(num,num){
+    num / num
 }
 
 
-let num1;
-let num2;
+let num;
 let operator;
 
 function operate(num1,num2,operator){
@@ -28,3 +27,29 @@ function operate(num1,num2,operator){
     if(operator == "/" || operator == "=") divide(num1,num2);
 }
 
+let numbers = document.querySelectorAll(".numbers");
+for(let i = 0; i < numbers.length; i++){
+    numbers[i].addEventListener("click",function(e){
+        num = Number(numbers[i].textContent);
+        let display = document.querySelector(".display");
+        display.textContent = num;
+
+    })
+}
+
+//Configure The Display
+
+let display = document.querySelector(".display");
+
+
+let operators = document.querySelectorAll(".operators")
+for(let i = 0 ; i < operators.length; i++){
+    operators[i].addEventListener("click",function(e){
+        operator = operators[i].textContent;
+        display.textContent = operator;
+})}
+
+let clear = document.querySelector('.clear')
+clear.addEventListener('click',function(){
+    display.textContent = "";
+})
